6629a15f596fc383cfbc17f7405c44d18bb5744a
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2024-03-11 17:15:46 +0200

    CRUD for POST

--- learn-express/routes.js
+++ learn-express/routes.js
@@ -17,12 +17,12 @@ router.get("/blogs", async (req, res) => {
 // Create post
 router.post("/blogs", async (req, res) => {
   try {
-    const post = new Blog({
+    const blog = new Blog({
       title: req.body.title,
       content: req.body.content,
     });
-    await post.save();
-    res.send(post);
+    await blog.save();
+    res.send(blog);
   } catch (error) {
     console.error("Error creating post:", error);
     res.status(500).send("Internal Server Error");
@@ -32,12 +32,12 @@ router.post("/blogs", async (req, res) => {
 // Get individual post
 router.get("/blogs/:id", async (req, res) => {
   try {
-    const post = await Post.findOne({ _id: req.params.id });
-    if (!post) {
+    const blog = await Blog.findOne({ _id: req.params.id });
+    if (!blog) {
       res.status(404).send({ error: "Blog not found" });
       return;
     }
-    res.send(post);
+    res.send(blog);
   } catch (error) {
     console.error("Error fetching post:", error);
     res.status(500).send("Internal Server Error");
@@ -47,8 +47,8 @@ router.get("/blogs/:id", async (req, res) => {
 // Update post
 router.patch("/blogs/:id", async (req, res) => {
   try {
-    const post = await Post.findOne({ _id: req.params.id });
-    if (!post) {
+    const blog = await Post.findOne({ _id: req.params.id });
+    if (!blog) {
       res.status(404).send({ error: "Post not found" });
       return;
     }
@@ -56,10 +56,10 @@ router.patch("/blogs/:id", async (req, res) => {
       post.title = req.body.title;
     }
     if (req.body.content) {
-      post.content = req.body.content;
+      blog.content = req.body.content;
     }
-    await post.save();
-    res.send(post);
+    await blog.save();
+    res.send(blog);
   } catch (error) {
     console.error("Error updating post:", error);
     res.status(500).send("Internal Server Error");
@@ -69,12 +69,12 @@ router.patch("/blogs/:id", async (req, res) => {
 // Delete post
 router.delete("/blogs/:id", async (req, res) => {
   try {
-    const post = await Post.findOne({ _id: req.params.id });
-    if (!post) {
+    const blog = await Blog.findOne({ _id: req.params.id });
+    if (!blog) {
       res.status(404).send({ error: "Post not found" });
       return;
     }
-    await Post.deleteOne({ _id: req.params.id });
+    await Blog.deleteOne({ _id: req.params.id });
     res.status(204).send();
   } catch (error) {
     console.error("Error deleting post:", error);
